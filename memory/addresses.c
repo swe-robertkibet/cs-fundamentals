425017f08983ef44b6f40de5741448f2870597fc
swe-robertkibet <swe.robertkibet@gmail.com>
2025-08-04 16:40:21 +0300

    Get and print the memory address to variable n

--- memory/addresses.c
+++ memory/addresses.c
@@ -3,5 +3,12 @@
 int main(void)
 {
     int n = 50;
+
+    // get the memory address for int n (wherever n is, get the address of it eg 0x45) and assign it to the variable p
+    int *p = &n;
+
     printf("%i\n", n);
+
+    // print the memory address to variable n
+    printf("%p\n", p);
 }
