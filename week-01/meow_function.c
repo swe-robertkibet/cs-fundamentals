0f2423e9aa63fc5d425cdaa120a86652d9c57ed0
swe-robertkibet <swe.robertkibet@gmail.com>
2025-08-27 22:24:47 +0300

    Add Week 1 C code examples

---
+++ week-01/meow_function.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(int n);
+
+int main(void)
+{
+    int n = get_int("Number: ");
+    meow(n);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
